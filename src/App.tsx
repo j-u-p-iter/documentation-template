3573fab70089db3e578fc703aed1937bd00e5be3
j.u.p.iter <pn79122821224@gmail.com>
2019-01-03 22:53:36 +0500

    adds some basic app styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Layout } from './Layout';
+import styles from './App.module.scss';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Layout />
+        <Layout  
+          wrapperClassName={styles.wrapper}
+          sidebarClassName={styles.sidebar}
+          contentClassName={styles.mainContent}
+          headerClassName={styles.header}
+          footerClassName={styles.footer}
+        />
       </BrowserRouter>
     );
   }
