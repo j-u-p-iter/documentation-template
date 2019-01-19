3963c371ac39e9736113afcc5b3ebd3193adeb33
j.u.p.iter <pn79122821224@gmail.com>
2019-01-19 19:17:49 +0500

    adds Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,5 +1,54 @@
 import * as React from 'react';
-import { Select } from '@j.u.p.iter/react-select';
+import { Select as ReactSelect } from '@j.u.p.iter/react-select';
+import classNames from 'classnames';
 
+import styles from './Select.module.scss';
 
-export { Select };
+export class Select extends React.Component {
+  state = {
+    isValid: true,
+    isMulti: false,
+    isOpened: false,
+  }
+
+  createStateToggler = (propName: string) => () => {
+    this.setState((state: any) => ({ [propName]: !state[propName] })); 
+  }
+
+  handleToggleMenu = (isOpened: boolean): void => {
+    this.setState({ isOpened });
+  }
+ 
+  render() {
+    const { isValid, isMulti, isOpened } = this.state;
+
+    const resultControlClassName = classNames(styles.control, { [styles.invalid]: !isValid, [styles.opened]: isOpened });
+
+    return (
+      <>
+        <ReactSelect 
+          containerClassName={styles.container}
+          menuClassName={styles.menu} 
+          controlClassName={resultControlClassName}
+          arrowClassName={styles.arrow}
+          labelClassName={styles.label}
+          placeholderClassName={styles.placeholder}
+          isMulti={isMulti}
+          onToggle={this.handleToggleMenu}
+        />
+        <div>
+          <label>
+            <input type='checkbox' onChange={this.createStateToggler('isMulti')} checked={isMulti} />
+            {' '}
+            isMulti
+          </label>
+        </div>
+        <div>
+          <input type='checkbox' onChange={this.createStateToggler('isValid')} checked={isValid} />
+          {' '}
+          isValid
+        </div>
+      </>
+    );
+  }
+} 
