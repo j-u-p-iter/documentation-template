abc34981f01b2f4b0e35c206bbab3a53d10b1f20
j.u.p.iter <pn79122821224@gmail.com>
2019-01-27 21:58:47 +0500

    extends CodeExample

--- src/CodeExample/CodeExample.tsx
+++ src/CodeExample/CodeExample.tsx
@@ -3,6 +3,52 @@ import { SwitcherController } from '@j.u.p.iter/react-switcher';
 
 import styles from './CodeExample.module.scss';
 
-export const CodeExample = () => (
-  <div>Hello</div>
-);
+interface PseudoEvent {
+  currentTarget: { 
+    name: string; 
+    value: boolean; 
+  };
+}
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+export class CodeExample extends React.Component<Props> {
+  renderDescription = () => {
+    const { description } = this.props;
+
+    return <div dangerouslySetInnerHTML={{ __html: description }} />
+  }
+
+  render() {
+    const { title, description, children } = this.props;
+
+    return (
+      <SwitcherController name='code-toggler' onChange={() => {}}>
+        {
+          ({ api: { getOn, toggleOn } }) => (
+            <div>
+              <header>
+                <h3>{title}</h3>
+
+                <button onClick={() => toggleOn()}>Toggler</button>
+              </header>
+
+              {
+                getOn() && (
+                  <div>Code</div>
+                )
+              }
+
+              {children}
+
+              {this.renderDescription()}
+            </div>
+          )
+        }
+      </SwitcherController>
+    );
+  }
+};
--- src/pages/components-pages/TagPage/TagPage.tsx
+++ src/pages/components-pages/TagPage/TagPage.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
+import { CodeExample } from '../../../CodeExample';
 import { Tag, Icon } from '../../../components';
 
 export const TagPage = () => {
   return (
-    <Tag isClearable>
-      some text
-    </Tag>
+    <CodeExample title='some title' description='some description'>
+      <Tag isClearable>some text</Tag>
+    </CodeExample>
   );
 };
